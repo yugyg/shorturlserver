897aa8cb85a409e0bf145e41534b48f25fd1446e
姜超 <40565202+jcobbery@users.noreply.github.com>
2018-11-16 18:01:13 +0800

    Update shorturlserver/src/main/java/com/yugyg/shorturlserver/SpringBootApplicationStart.java
    启动类删除@EnableAutoConfiguration

--- shorturlserver/src/main/java/com/yugyg/shorturlserver/SpringBootApplicationStart.java
+++ shorturlserver/src/main/java/com/yugyg/shorturlserver/SpringBootApplicationStart.java
@@ -5,14 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.yugyg.*")
 @MapperScan(value = "com.yugyg.*mapper")
-@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootApplicationStart implements CommandLineRunner
 {
